db0b03483b0c34ef9b161ccb93b59603b7e5205f
adsl58462 <81452903+adsl58462@users.noreply.github.com>
2025-04-14 11:18:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,303 @@
+const games = {
+  "<  45分": [
+    {
+      "name": "德國心臟病",
+      "players": "4+"
+    },
+    {
+      "name": "機密代號圖案版",
+      "players": "4+"
+    },
+    {
+      "name": "終極密碼",
+      "players": "4"
+    },
+    {
+      "name": "伐木工",
+      "players": "3+"
+    },
+    {
+      "name": "抓鬼大隊",
+      "players": "nan"
+    },
+    {
+      "name": "一夜抵抗組織",
+      "players": "nan"
+    },
+    {
+      "name": "一夜吸血鬼",
+      "players": "nan"
+    },
+    {
+      "name": "卡卡頌新版",
+      "players": "2019-02-06 00:00:00"
+    },
+    {
+      "name": "阿瓦隆可愛版",
+      "players": "2019-05-10 00:00:00"
+    },
+    {
+      "name": "亞瑟傳奇",
+      "players": "2022-04-10 00:00:00"
+    },
+    {
+      "name": "恐怖村",
+      "players": "nan"
+    },
+    {
+      "name": "MiniPark",
+      "players": "nan"
+    },
+    {
+      "name": "Exit第一章",
+      "players": "nan"
+    },
+    {
+      "name": "惡魔島",
+      "players": "2019-03-05 00:00:00"
+    },
+    {
+      "name": "多米諾王國",
+      "players": "4"
+    },
+    {
+      "name": "花磚物語",
+      "players": "2019-02-04 00:00:00"
+    },
+    {
+      "name": "花磚物語2",
+      "players": "nan"
+    },
+    {
+      "name": "告白",
+      "players": "nan"
+    },
+    {
+      "name": "哆寶",
+      "players": "2020-02-08 00:00:00"
+    },
+    {
+      "name": "獨家暗語",
+      "players": "7"
+    },
+    {
+      "name": "試個好遊戲",
+      "players": "10"
+    },
+    {
+      "name": "搞笑瑜珈",
+      "players": "3-10+"
+    },
+    {
+      "name": "魔語地城",
+      "players": "2022-04-08 00:00:00"
+    },
+    {
+      "name": "欲罷不能",
+      "players": "4"
+    },
+    {
+      "name": "得分沙拉",
+      "players": "6"
+    },
+    {
+      "name": "驚爆倫敦",
+      "players": "2022-04-08 00:00:00"
+    },
+    {
+      "name": "量化寬鬆",
+      "players": "2022-03-05 00:00:00"
+    },
+    {
+      "name": "閃靈快手",
+      "players": "2022-02-08 00:00:00"
+    }
+  ],
+  "45min~90min": [
+    {
+      "name": "拉密Rummikub",
+      "players": "2019-02-04 00:00:00"
+    },
+    {
+      "name": "璀璨寶石 x3",
+      "players": "2019-02-04 00:00:00"
+    },
+    {
+      "name": "山中小屋擴充",
+      "players": "nan"
+    },
+    {
+      "name": "王牌檢察官",
+      "players": "2022-02-06 00:00:00"
+    },
+    {
+      "name": "七大奇蹟-對決及擴充",
+      "players": "2"
+    },
+    {
+      "name": "Abolition Now!\n廢除！現在！",
+      "players": "nan"
+    },
+    {
+      "name": "拯救北極熊",
+      "players": "2019-03-04 00:00:00"
+    },
+    {
+      "name": "職場地下城",
+      "players": "2019-03-05 00:00:00"
+    },
+    {
+      "name": "骰子馬戲團",
+      "players": "4"
+    },
+    {
+      "name": "卡卡頌bigbox",
+      "players": "2019-02-06 00:00:00"
+    },
+    {
+      "name": "瑪利歐地產大亨",
+      "players": "2019-03-04 00:00:00"
+    },
+    {
+      "name": "璀燦寶石之城市擴充",
+      "players": "2019-02-04 00:00:00"
+    },
+    {
+      "name": "奶油還是派",
+      "players": "2020-02-05 00:00:00"
+    },
+    {
+      "name": "律師大爆炸",
+      "players": "8"
+    },
+    {
+      "name": "綿羊爭牧場",
+      "players": "2020-02-04 00:00:00"
+    },
+    {
+      "name": "動物朋友",
+      "players": "2020-03-06 00:00:00"
+    },
+    {
+      "name": "奇思謬想",
+      "players": "2020-02-12 00:00:00"
+    },
+    {
+      "name": "賭命大賽",
+      "players": "5人"
+    },
+    {
+      "name": "妙探尋兇（辛普森家族版）",
+      "players": "6人"
+    },
+    {
+      "name": "衝向黃金城",
+      "players": "2-4人"
+    },
+    {
+      "name": "熊熊公園",
+      "players": "2022-02-04 00:00:00"
+    }
+  ],
+  ">  90min": [
+    {
+      "name": "追求幸福人生",
+      "players": "4"
+    },
+    {
+      "name": "大五月花號",
+      "players": "2019-03-05 00:00:00"
+    },
+    {
+      "name": "商人擴充等",
+      "players": "nan"
+    },
+    {
+      "name": "深水城領主",
+      "players": "nan"
+    },
+    {
+      "name": "仙境幽谷",
+      "players": "4"
+    },
+    {
+      "name": "高雄大空襲",
+      "players": "2021-02-06 00:00:00"
+    },
+    {
+      "name": "光明城市",
+      "players": "4-6人"
+    },
+    {
+      "name": "馬約利卡",
+      "players": "2025-02-04 00:00:00"
+    },
+    {
+      "name": "超級犀牛",
+      "players": "2025-02-06 00:00:00"
+    },
+    {
+      "name": "猜心俱樂部",
+      "players": "2025-03-08 00:00:00"
+    },
+    {
+      "name": "首都華沙",
+      "players": "2025-02-04 00:00:00"
+    },
+    {
+      "name": "蜜蜂花園",
+      "players": "2025-02-04 00:00:00"
+    },
+    {
+      "name": "青青珊瑚島",
+      "players": "2025-02-04 00:00:00"
+    },
+    {
+      "name": "漢薩同盟",
+      "players": "2025-02-05 00:00:00"
+    },
+    {
+      "name": "數字九層塔",
+      "players": "2025-02-04 00:00:00"
+    },
+    {
+      "name": "四季之森",
+      "players": "2025-02-04 00:00:00"
+    },
+    {
+      "name": "最後的星期五",
+      "players": "2025-02-05 00:00:00"
+    },
+    {
+      "name": "天際線3000",
+      "players": "2025-02-04 00:00:00"
+    }
+  ],
+  "nan": [
+    {
+      "name": "娃娃",
+      "players": "nan"
+    }
+  ]
+};
+
+
+function draw() {
+  const time = document.getElementById('time').value;
+  const resultDiv = document.getElementById('result');
+
+  if (!time) {
+    resultDiv.innerHTML = '請先選擇遊戲時間喔！';
+    return;
+  }
+
+  const gameList = games[time];
+  const game = gameList[Math.floor(Math.random() * gameList.length)];
+
+  let text = `抽到：${game.name}`;
+  if (game.players) {
+    text += ` <br>適合人數：${game.players}`;
+  }
+
+  resultDiv.innerHTML = text;
+}
